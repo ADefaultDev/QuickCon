d755dda8e8be74f27189e33f14436e818c89c947
ADefaultDev <im7ckngmad@gmail.com>
2021-10-31 00:51:49 +0600

    Test2
    Signed-off-by: ADefaultDev <im7ckngmad@gmail.com>

--- src/DatabaseManager.java
+++ src/DatabaseManager.java
@@ -45,7 +45,6 @@ class DatabaseManager {
 
     private void readDatabaseProperties() throws IOException {
         props = new Properties();
-
         try {
             try (InputStream in = Files.newInputStream(Paths.get(Objects.requireNonNull(this.getClass().getClassLoader().getResource("database.properties")).toURI()))) {
                 props.load(in);
